3b01a06fbab95787a9381a1979a88425e9000215
Andrei Sold <sold_andrei@yahoo.com>
2022-10-15 13:56:40 +0300

    Initial app

---
+++ README.md
@@ -0,0 +1,1 @@
+# This project has been created solely for Azure DevOps demonstration purposes within msg's Halloween MeetUp.
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="background">
+      <img className="ghost" src='./casper.png' alt="casper"/>
+    </div>
+  );
+}
+
+export default App;
